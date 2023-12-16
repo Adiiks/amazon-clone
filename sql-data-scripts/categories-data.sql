e59c5da141a64e369b9a8b758515d6db68d6af3a
Adiks <apacholak97@interia.pl>
2023-12-16 14:06:30 +0100

    adding category-data-script

---
+++ sql-data-scripts/categories-data.sql
@@ -0,0 +1,20 @@
+INSERT INTO category (name)
+VALUES ('Books');
+
+INSERT INTO category (name)
+VALUES ('Computers');
+
+INSERT INTO category (name)
+VALUES ('Movies');
+
+INSERT INTO category (name)
+VALUES ('Music');
+
+INSERT INTO category (name)
+VALUES ('Pet Supplies');
+
+INSERT INTO category (name)
+VALUES ('Sport');
+
+INSERT INTO category (name)
+VALUES ('Video Games');
