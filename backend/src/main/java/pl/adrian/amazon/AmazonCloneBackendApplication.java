67d81709d522656b2351128c9df9d269d858fbc7
Adiks <apacholak97@interia.pl>
2023-12-16 11:23:56 +0100

    created initial backend

---
+++ backend/src/main/java/pl/adrian/amazon/AmazonCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.adrian.amazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonCloneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonCloneBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pl/adrian/amazon/AmazonCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adrian.amazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonCloneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
