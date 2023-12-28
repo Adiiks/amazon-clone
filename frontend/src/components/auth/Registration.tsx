ec1897205ec5914c024f6a7eb91a41c724111696
Adiks <apacholak97@interia.pl>
2023-12-28 14:10:17 +0100

    frontend: adding registration functionality

--- frontend/src/components/auth/Registration.tsx
+++ frontend/src/components/auth/Registration.tsx
@@ -1,38 +1,116 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import styles from './auth.module.css';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { backendUrl } from '../../environments';
+import toast from 'react-hot-toast';
+
+type FormValues = {
+    name: string,
+    email: string,
+    password: string,
+    reEnteredPassword: string
+}
 
 const Registration = () => {
+    const { register, handleSubmit, setError, formState: { errors } } = useForm<FormValues>();
+    const navigate = useNavigate();
+
+    function onSubmit(data: FormValues) {
+        const url = `${backendUrl}auth/registration`;
+        const payload = {
+            fullName: data.name,
+            email: data.email,
+            password: data.password
+        }
+
+        axios.post(url, payload)
+            .then(() => {
+                navigate('/auth/sign-in');
+            })
+            .catch(({ response }) => {
+                if (response.status === 409) {
+                    setError('email', {
+                        type: 'custom',
+                        message: 'There\'s already an account with this email.'
+                    });
+                } else {
+                    toast.error('Something went wrong !');
+                }
+            });
+    }
+
     return (
         <div className={styles['auth-container']}>
             <h1>Create account</h1>
 
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div className={styles['input-wrapper']}>
                     <label htmlFor="name">Your name</label>
-                    <input type="text" id='name' placeholder='First and last name' />
+                    <input
+                        type="text"
+                        id='name'
+                        className={errors.name && styles['input-error']}
+                        placeholder='First and last name'
+                        {...register('name', { required: 'Enter your name' })}
+                    />
+                    <p className={styles.error}>{errors.name && errors.name.message}</p>
                 </div>
 
                 <div className={styles['input-wrapper']}>
                     <label htmlFor="email">Email</label>
-                    <input type='email' id='email' />
+                    <input
+                        type='text'
+                        id='email'
+                        className={errors.email && styles['input-error']}
+                        {...register('email', {
+                            required: 'Enter your email',
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                message: 'Invalid email address. Please correct and try again.'
+                            }
+                        })}
+                    />
+                    <p className={styles.error}>{errors.email && errors.email.message}</p>
                 </div>
 
                 <div className={styles['input-wrapper']}>
                     <label htmlFor="password">Password</label>
-                    <input type="password" id='password' placeholder='At least 6 characters' />
+                    <input
+                        type="password"
+                        id='password'
+                        placeholder='At least 6 characters'
+                        className={errors.password && styles['input-error']}
+                        {...register('password', {
+                            required: 'Minimum 6 characters required',
+                            minLength: {
+                                value: 6,
+                                message: 'Minimum 6 characters required'
+                            }
+                        })}
+                    />
+                    <p className={styles.error}>{errors.password && errors.password.message}</p>
                 </div>
 
                 <div className={styles['input-wrapper']}>
                     <label htmlFor="re-enter-password">Re-enter password</label>
-                    <input type="password" id='re-enter-password' />
+                    <input
+                        type="password"
+                        id='re-enter-password'
+                        className={errors.reEnteredPassword && styles['input-error']}
+                        {...register('reEnteredPassword', {
+                            validate: (value, formValues) => value === formValues.password || 'Passwords must match'
+                        })}
+                    />
+                    <p className={styles.error}>{errors.reEnteredPassword && errors.reEnteredPassword.message}</p>
                 </div>
 
-                <button type='button'>Continue</button>
+                <button type='submit'>Continue</button>
             </form>
 
             <hr />
             <p>
-                Already have an account? <Link to='/auth/sign-in'>Sign in</Link> 
+                Already have an account? <Link to='/auth/sign-in'>Sign in</Link>
             </p>
         </div>
     )
